ebe5558efa6a7dfb38b44c4434a23e49d886944c
goldinvo <goldinbaokimvo@gmail.com>
2021-06-29 13:30:08 -0500

    implement ManualCounter component

---
+++ src/components/ManualCounter.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import Counter from "./Counter"
+
+const maxCount = 999;
+
+// props:
+// state: count is always a string that is either empty or represents a 3 digit whole number
+class ManualCounter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: '0',
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.increment = this.increment.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    handleChange(e) {
+        let newCount = e.target.value;
+        if (newCount.match(/\D/)) {
+            // Don't accept non-numeric chars
+            console.log('jo');
+            return;
+        }    
+        newCount = parseInt(newCount); // NaN if empty string
+        this.setState({
+            count: newCount ? newCount : "", 
+        })
+    }
+
+    increment(i) {
+        let newCount = Number(this.state.count) + i; // Number('') === 0
+        this.setState({
+            count: newCount <= maxCount 
+                ? "" + newCount 
+                : "" + (newCount - maxCount - 1),
+        })
+    }
+
+    reset(){
+        this.setState({count: '0'});
+    }
+
+    render() {
+        return (
+            <div>
+                <Counter name="Manual Counter" onChange={this.handleChange} value={this.state.count} />
+                <button type='button' onClick={() => this.increment(1)}>+1</button>
+                <button type='button' onClick={() => this.increment(10)}>+10</button>
+                <button type='button' onClick={this.reset}>Reset</button>
+            </div>
+        );
+    }
+}
+
+export default ManualCounter;
