06ab4e3e9facbe7222bdf1412821159461eedeb9
goldinvo <goldinbaokimvo@gmail.com>
2021-07-10 19:37:19 -0500

    repeat counters

---
+++ src/components/InstructionText.js
@@ -0,0 +1,46 @@
+import {TokenType} from '../Token'
+import './InstructionText.css';
+
+// properties: instruction, index, colors
+function InstructionText(props) {
+  let indicesToStyle = [];
+  props.instruction.forEach((token, index) => {
+    if (token.type === TokenType.OPN_PAREN && index < props.index) {
+      indicesToStyle.push(index);
+    } else if (token.type === TokenType.CLS_PAREN) {
+      index < props.index ? indicesToStyle.pop() : indicesToStyle.push(index);
+    }
+  });
+
+
+  let colorIndex = 0;    
+  let clsParenColorStack = [];
+  let output = props.instruction.map( (token, index) => {
+    
+    const currentInstr = index === props.index;
+    const coloredParen = indicesToStyle.includes(index);
+    let classes = currentInstr ? 'focused ' : '';
+    if (coloredParen) {
+      if (token.type === TokenType.OPN_PAREN) {
+        classes += props.colors[colorIndex];
+        clsParenColorStack.push(props.colors[colorIndex]);
+        
+        if ((colorIndex + 1) < props.colors.length) colorIndex++;
+      } else {
+        // closed paren
+        classes += clsParenColorStack.pop();
+      }
+      
+    }
+
+    return <span className={classes} key={index}>{token.value + " "}</span>;
+  }) 
+  
+  return (
+    <div>
+      {output}
+    </div>
+  );
+}
+
+export default InstructionText;
