93a922cdcf3e0e74ab731b9b873da2d08f830c8f
goldinvo <goldinbaokimvo@gmail.com>
2024-07-03 23:25:40 -0500

    fix dead link and package stuff

--- src/index.js
+++ src/index.js
@@ -14,11 +14,8 @@ ReactDOM.render(
       info={
         <div>
           <p>
-            Start by putting inputting your pattern using the pattern counter syntax. 
-            Visit my <a href='https://goldinvo.com/2021/07/22/pattern-counter.html' target='_blank' rel="noreferrer">blog post</a> for examples and usage details.
-          </p>
-          <p>
-            Please send any questions, comments/feedback, or bug reports my way! I would much appreciate it: goldin@goldinvo.com
+            Start by inputting your pattern using the pattern counter syntax. 
+            Visit my <a href='https://goldinvo.com/blog/2021/07/22/pattern-counter.html' target='_blank' rel="noreferrer">blog post</a> for examples and usage details.
           </p>
         </div>   
       }
